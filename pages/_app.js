c42880149998361bff2bc826796c87035b329a41
mittya6 <53619699+mittya6@users.noreply.github.com>
2025-07-09 23:28:19 +0900

    Add highlight.js initialization

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,12 @@
 import '../styles/global.css';
 import 'highlight.js/styles/github-dark.css';
+import { useEffect } from 'react';
+import hljs from 'highlight.js';
 
 export default function App({ Component, pageProps }) {
+  useEffect(() => {
+    hljs.highlightAll();
+  }, []);
+
   return <Component {...pageProps} />;
 }
